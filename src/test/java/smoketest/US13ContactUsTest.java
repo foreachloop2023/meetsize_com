2f1b09f7edcb1cb91d4e155e7ee93386dbc336a9
foreachloop2023 <foreachloop2023@gmail.com>
2020-09-08 00:38:41 +0300

    9 eylul birlesik hali

--- src/test/java/smoketest/US13ContactUsTest.java
+++ src/test/java/smoketest/US13ContactUsTest.java
@@ -9,10 +9,11 @@ import utilities.Driver;
 import utilities.TestBaseFinal;
 
 public class US13ContactUsTest extends TestBaseFinal {
+    US13ContactUsPages us13ContactUsPages;
 
     @Test
     public void contactUsTest() throws InterruptedException {
-        US13ContactUsPages us13ContactUsPages=new US13ContactUsPages();
+        us13ContactUsPages = new US13ContactUsPages();
 
         driver.get("https://meetsize.com");
 
@@ -26,14 +27,14 @@ public class US13ContactUsTest extends TestBaseFinal {
 
         Thread.sleep(3000);
 
-       JavascriptExecutor js=((JavascriptExecutor)Driver.getdriver());
-       js.executeScript("arguments[0].click();",us13ContactUsPages.contactUs);
-       us13ContactUsPages.name.sendKeys(ConfigReader.getProperty("name"));
-       us13ContactUsPages.lastname.sendKeys(ConfigReader.getProperty("lastname"));
-       us13ContactUsPages.phone.sendKeys(ConfigReader.getProperty("phone"));
-       us13ContactUsPages.emailContactUs.sendKeys(ConfigReader.getProperty("email"));
-       us13ContactUsPages.messageContactUs.sendKeys(ConfigReader.getProperty("message"));
-       us13ContactUsPages.sendContact.click();
+        JavascriptExecutor js = ((JavascriptExecutor) Driver.getdriver());
+        js.executeScript("arguments[0].click();", us13ContactUsPages.contactUs);
+        us13ContactUsPages.name.sendKeys(ConfigReader.getProperty("name"));
+        us13ContactUsPages.lastname.sendKeys(ConfigReader.getProperty("lastname"));
+        us13ContactUsPages.phone.sendKeys(ConfigReader.getProperty("phone"));
+        us13ContactUsPages.emailContactUs.sendKeys(ConfigReader.getProperty("email"));
+        us13ContactUsPages.messageContactUs.sendKeys(ConfigReader.getProperty("message"));
+        us13ContactUsPages.sendContact.click();
         Assert.assertTrue(us13ContactUsPages.messageSentSuccessfully.isDisplayed());
 
     }
